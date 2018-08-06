5a4b72d371763080bea79cbaeb66a9c2fc73245c
kalanithy <fahadmoinuddin@yahoo.com>
2018-08-06 15:47:04 +0530

    Adding settings page

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Login from "./containers/Login";
 import SignUp from "./containers/SignUp";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute"
@@ -15,6 +16,7 @@ export default({ childProps }) =>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
         <UnauthenticatedRoute path="/login" exact component ={Login} props={childProps} />
         <UnauthenticatedRoute path="/signup" exact component ={SignUp} props={childProps} />
+        <AuthenticatedRoute path="/settings" exact component = {Settings} props={childProps} />
         <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
         <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
         <Route component={NotFound} />
